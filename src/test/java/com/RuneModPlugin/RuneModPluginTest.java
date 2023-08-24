99416e9e68297f2e2f47357e94c4307005384a4f
Freewillyy <Washingtonog570@outlook.co>
2023-08-23 17:30:02 -0700

    RuneMod Plugin for Runelite

--- src/test/java/com/RuneModPlugin/RuneModPluginTest.java
+++ src/test/java/com/RuneModPlugin/RuneModPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.RuneModPlugin;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RuneModPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(RuneModPlugin.class);
 		RuneLite.main(args);
 	}
 }
